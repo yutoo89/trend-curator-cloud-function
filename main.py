713dddb2ff04533cf12667250068d018fd1f65b0
yutoo89 <yuta.o.note@gmail.com>
2025-01-12 13:41:16 +0900

    add on_user_trend_update_started

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ from topic import Topic
 from trend import Trend
 from access_updater import AccessUpdater
 from web_searcher import WebSearcher
+from user_trend_update_publisher import UserTrendUpdatePublisher
+import base64
+import json
 
 # GenAI 初期化
 genai.configure(api_key=os.environ["GENAI_API_KEY"])
@@ -72,32 +75,42 @@ def on_topic_created(cloud_event: CloudEvent) -> None:
 
 @functions_framework.cloud_event
 def on_trend_update_started(cloud_event):
-    from user_trend_update_publisher import UserTrendUpdatePublisher
-    published_user_count = UserTrendUpdatePublisher().fetch_and_publish()
+    """
+    trend-updatesトピックにメッセージが送信された時に実行
+    """
+
+    UserTrendUpdatePublisher().fetch_and_publish()
 
 
 @functions_framework.cloud_event
 def on_user_trend_update_started(cloud_event):
     """
-    「ユーザトレンドの更新が開始したとき」に実行。
-    メッセージに含まれるユーザIDを基にニュースを作成。
+    user-trend-updatesトピックにメッセージが送信された時に実行
     """
-    import base64
 
-    # CloudEventデータを取得
+    print(f"[INFO] Processing on_user_trend_update_started - Start")
     pubsub_message = cloud_event.data
-    if "message" in pubsub_message and "data" in pubsub_message["message"]:
-        # メッセージデータをデコード
-        message_data = base64.b64decode(pubsub_message["message"]["data"]).decode("utf-8")
-        print(f"Received message: {message_data}")
-        
-        # ユーザIDを取得
-        import json
-        message_json = json.loads(message_data)
-        user_id = message_json.get("user_id", None)
-        if user_id:
-            print(f"Generating news for user: {user_id}")
-        else:
-            print("No user_id found in message.")
-    else:
+
+    if "message" not in pubsub_message or "data" not in pubsub_message["message"]:
         print("Invalid Pub/Sub message format.")
+        return
+
+    message_data = base64.b64decode(pubsub_message["message"]["data"]).decode("utf-8")
+    message_json = json.loads(message_data)
+    user_id = message_json.get("user_id", None)
+
+    if not user_id:
+        print("No user_id found in message.")
+        return
+
+    topic = Topic.find(db, user_id)
+    searcher = WebSearcher(google_custom_search_api_key, google_search_cse_id)
+    trend = Trend.update(
+        db,
+        user_id,
+        topic.topic,
+        topic.language_code,
+        searcher,
+        topic.exclude_keywords,
+    )
+    Topic.update_exclude_keywords(db, user_id, trend.keywords)
--- topic.py
+++ topic.py
@@ -2,39 +2,75 @@ from __future__ import annotations
 from firebase_admin import firestore
 from gemini_text_corrector import GeminiTextCorrector
 
+
 class Topic:
-    def __init__(self, user_id: str, raw_topic: str, topic: str, language_code: str, region_code: str):
+    def __init__(
+        self,
+        user_id: str,
+        raw_topic: str,
+        topic: str,
+        language_code: str,
+        region_code: str,
+        exclude_keywords: list[str] = None,
+    ):
+        if exclude_keywords is None:
+            exclude_keywords = []
         self.user_id = user_id
         self.raw_topic = raw_topic
         self.topic = topic
         self.language_code = language_code
         self.region_code = region_code
+        self.exclude_keywords = exclude_keywords
 
     @staticmethod
-    def update(db: firestore.Client, user_id: str, raw_topic: str, locale: str) -> Topic:
-        parts = locale.split('-')
+    def find(db: firestore.Client, user_id: str) -> Topic:
+        topic_data = db.collection("topics").document(user_id).get().to_dict()
+        return Topic(
+            user_id,
+            topic_data.get("raw_topic"),
+            topic_data.get("topic"),
+            topic_data.get("language_code"),
+            topic_data.get("region_code"),
+            topic_data.get("exclude_keywords"),
+        )
+
+    @staticmethod
+    def update(
+        db: firestore.Client, user_id: str, raw_topic: str, locale: str
+    ) -> Topic:
+        parts = locale.split("-")
         if len(parts) != 2:
-            raise ValueError("Invalid locale format. Expected format: 'language-region'.")
+            raise ValueError(
+                "Invalid locale format. Expected format: 'language-region'."
+            )
         language_code, region_code = parts
 
         corrector = GeminiTextCorrector("gemini-1.5-flash")
         corrected_topic = corrector.run(raw_topic, region_code)
 
         doc_ref = db.collection("topics").document(user_id)
-        doc_ref.update({
-            "topic": corrected_topic,
-            "language_code": language_code,
-            "region_code": region_code,
-        })
+        doc_ref.update(
+            {
+                "topic": corrected_topic,
+                "language_code": language_code,
+                "region_code": region_code,
+            }
+        )
 
         return Topic(user_id, raw_topic, corrected_topic, language_code, region_code)
 
     @staticmethod
-    def update_exclude_keywords(db: firestore.Client, user_id: str, exclude_keywords: list[str]) -> None:
-        if not isinstance(exclude_keywords, list) or not all(isinstance(k, str) for k in exclude_keywords):
+    def update_exclude_keywords(
+        db: firestore.Client, user_id: str, exclude_keywords: list[str]
+    ) -> None:
+        if not isinstance(exclude_keywords, list) or not all(
+            isinstance(k, str) for k in exclude_keywords
+        ):
             raise ValueError("exclude_keywords must be a list of strings.")
 
         doc_ref = db.collection("topics").document(user_id)
-        doc_ref.update({
-            "exclude_keywords": exclude_keywords,
-        })
+        doc_ref.update(
+            {
+                "exclude_keywords": exclude_keywords,
+            }
+        )
